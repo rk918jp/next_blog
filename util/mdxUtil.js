ebbbef569712d94e19ef972b0f60325ca0db338c
rk918jp <rk918jp@users.noreply.github.com>
2022-10-25 18:27:47 +0900

    misc

--- util/mdxUtil.js
+++ util/mdxUtil.js
@@ -5,27 +5,6 @@ import {remark} from "remark";
 import remarkHtml from "remark-html";
 import moment from "moment";
 
-export const getPostsByCategory = async (category) => {
-  const files = await recursive(`mdx-contents/${category}`);
-  return files.map((path) => {
-    const [category, slug] = path
-      .replace("mdx-contents/", "")
-      .replace(".mdx", "")
-      .split("/");
-    const file = fs.readFileSync(`mdx-contents/${category}/${slug}.mdx`);
-    const {data: metadata} = matter(file);
-    return {
-      category,
-      slug,
-      metadata,
-      path: `/${category}/${slug}`,
-    };
-  })
-    .sort((a,b) => {
-      return moment(a.metadata.publishedAt).isAfter(b.metadata.publishedAt) ? -1 : 1;
-    });
-};
-
 export const getPosts = async () => {
   const files = await recursive(`mdx-contents`);
   return files.map((path) => {
@@ -59,26 +38,6 @@ export const getPost = async (category, slug) => {
     contentMd: mdString,
   }
 };
-
-export const createPost = async (content, metadata) => {
-  const mdxString = matter.stringify(content, metadata);
-  const dirPath = `mdx-contents/${metadata.category}`
-  const filePath = `${dirPath}/${metadata.slug}.mdx`;
-
-  const dirExists = fs.existsSync(dirPath);
-  if (!dirExists) {
-    fs.mkdirSync(dirPath);
-  }
-
-  const fileExists = fs.existsSync(filePath);
-  if (fileExists) {
-    throw new Error("Duplicate post");
-  }
-
-  fs.writeFileSync(filePath, mdxString);
-  return true;
-}
-
 
 export const updatePost = async (content, metadata) => {
   const mdxString = matter.stringify(content, metadata);
