2fb4926f4ebc4182468d88b51365e6d0bfb735c2
rk918jp <113189714+rk918jp@users.noreply.github.com>
2023-07-30 23:26:49 +0900

    単体テスト修正

--- util/date.test.js
+++ util/date.test.js
@@ -1,11 +1,11 @@
 import {formatDateTime} from "./date";
 
 describe("formatDateTime", () => {
-  test("デフォルトのフォーマットで変換", () => {
+  test("デフォルトのフォーマットで変換(UTC -> JST)", () => {
     expect(formatDateTime("2023-01-02T03:45.678Z")).toBe("2023/01/02 12:45")
   });
 
-  test("入力のフォーマットを指定", () => {
+  test("入力のフォーマットを指定(JST -> JST)", () => {
     expect(formatDateTime(
       "20230102_0345_678",
       {
@@ -14,7 +14,7 @@ describe("formatDateTime", () => {
       )).toBe("2023/01/02 03:45")
   });
 
-  test("出力のフォーマットを指定", () => {
+  test("出力のフォーマットを指定(UTC -> JST)", () => {
     expect(formatDateTime(
       "2023-01-02T03:45.678Z",
       {
